e5515e362d7b7972610ba62a1c3f6de58b808427
Reece <reececottam12@msn.com>
2022-08-20 21:04:20 +0100

    WiP

--- WeatherConversion.Core/Program.cs
+++ WeatherConversion.Core/Program.cs
@@ -16,24 +16,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/TempConvert", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return ("Hello World!");
+   
 })
-.WithName("GetWeatherForecast");
+.WithName("GetConverter");
 
 app.Run();
 
---
+++ WeatherConversion.Core/TempConverterServices.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using UTL = UltimateTemperatureLibrary;
+
+
+namespace WeatherConversion.Core
+{
+    public class TempConverterServices
+    {
+        public string Convert(double temperature, char unitFrom, char unitTo)
+        {
+
+            //use UTL and convert accordingly
+
+            return "";
+
+
+
+        }
+
+    }
+}
