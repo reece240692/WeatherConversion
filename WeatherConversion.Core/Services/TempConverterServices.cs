a3bc8b249d01d9ffa3f31a6da005198ab52ec151
Reece <reececottam12@msn.com>
2022-08-22 08:56:18 +0100

    First Iteration passes all tests...need to smoke test and build front end now

--- WeatherConversion.Core/Services/TempConverterServices.cs
+++ WeatherConversion.Core/Services/TempConverterServices.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.CompilerServices;
-using UTL = UltimateTemperatureLibrary;
+﻿using UTL = UltimateTemperatureLibrary.Converter;
 
 
 namespace WeatherConversion.Core.Services
@@ -13,9 +10,32 @@ namespace WeatherConversion.Core.Services
 
             //use UTL and convert accordingly
 
+            if (unitFrom =='F' && unitTo =='C')
+            {
+                return UTL.Fah2Cel(temperature).ToString("F0");
+            }
+            if (unitFrom == 'F' && unitTo == 'K')
+            {
+                return UTL.Fah2Kel(temperature).ToString("F0");               
+            }
+            if (unitFrom == 'K' && unitTo == 'F')
+            {
+                return UTL.Kel2Fah(temperature).ToString("F0");
+            }
+            if (unitFrom == 'K' && unitTo == 'C')
+            {
+                return UTL.Kel2Cel(temperature).ToString("F0");
+            }
+            if (unitFrom == 'C' && unitTo == 'F')
+            {
+                return UTL.Cel2Fah(temperature).ToString("F0");
+            }
+            if (unitFrom == 'C' && unitTo == 'K')
+            {
+                return UTL.Cel2Kel(temperature).ToString("F0");
+            }
+            else //If we got this far, something went wrong!
             return "";
-
-
 
         }
 
